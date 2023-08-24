c79818cfe5b5a37cf8cba5cefdb591e1456ba7cf
jjcl16 <caraballo2j@gmail.com>
2023-08-24 17:58:07 -0300

    Initiated the project

---
+++ index.bundle.js
@@ -0,0 +1,19 @@
+(self["webpackChunkbattleship"] = self["webpackChunkbattleship"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log("runniiiiiing")
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9iYXR0bGVzaGlwLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwicnVubmlpaWlpaW5nXCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
