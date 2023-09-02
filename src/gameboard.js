8b6268f5dbabef69796a1650a60f939ec85a2c5e
jjcl16 <caraballo2j@gmail.com>
2023-09-02 19:09:45 -0300

    test refactor, individual ship on gameboard. Gameboard attack return ship attacked

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,4 @@
+
 const Gameboard = (x,y) => {
   const xSize = x;
   const ySize = y;
@@ -12,35 +13,61 @@ const Gameboard = (x,y) => {
   const placeShip = (ship, xPosition = 0, yPosition = 0, orientation = "v") => {
     const shipLength = ship.shipSize;
     if(orientation === "v" && xPosition < xSize){
-      if ((yPosition + shipLength) <= (ySize)){
-        insertShip(xPosition, yPosition, orientation, shipLength);
+      if ((yPosition + shipLength) <= (ySize)){        
+        insertShip(xPosition, yPosition, orientation, ship);
         return board;
-      }else{
-        return false
       }
     } else if(orientation === "h" && yPosition < ySize){
       if ((xPosition + shipLength) <= (xSize)){
-        insertShip(yPosition, xPosition, orientation, shipLength);
+        insertShip(yPosition, xPosition, orientation, ship);
         return board;
-      }else{
-        return false
-      }
-    }else return false;
-  }
-
-  const receiveAttack  = (xPosition = 0, yPosition = 0) => {
-
-  }
-
-  const insertShip = (atFixed, atBeginning, orientation = "v", size) => {
-    for(let i = atBeginning; i < atBeginning+size ; i++ ){
-        if (orientation === "h"){
-        board[i][atFixed] = "SHIP"
-      } else{
-        board[atFixed][i] = "SHIP"
       }
     }
+    return false;
+  }
+
+   const receiveAttack  = (x = 0, y = 0) => {
+    if(typeof(board[x][y]) === "object") {
+      const shipAttacked = board[x][y];
+      shipAttacked.hit();
+      board[x][y] = "X";
+      return shipAttacked;
+    } else{      
+      board[x][y] = "X";
+      return false;
+    } 
+   }
+
+  const insertShip = (atFixed, atBeginning, orientation = "v", ship) => {
+    let shipSize = ship.shipSize;
+    let isThereAShip = alreadyAShip( atFixed, atBeginning, orientation, shipSize);
+     //console.log({ship})
+    if(!isThereAShip){
+      
+      for(let i = atBeginning; i < (atBeginning + shipSize) ; i++ ){
+        if (orientation === "h"){          
+          board[i][atFixed] = ship;
+        } else{
+          board[atFixed][i] = ship;          
+        }
+      }
+    }
+    //console.log(board)
+  }
+
+  const alreadyAShip = (atFixed, atBeginning, orientation = "v", shipSize) => {
     
+    //console.log(board);
+    let checkShips = false;
+    for(let i = atBeginning; i < (atBeginning + shipSize) ; i++ ){
+      //console.log(board);
+      if (orientation === "h" && typeof(board[i][atFixed]) === "object"){
+          checkShips = true;
+      } else if(orientation === "v" && typeof(board[atFixed][i]) === "object"){
+        checkShips = true;
+      }    
+    }
+    return checkShips;
   }
 
   return { board, placeShip, receiveAttack }
@@ -49,10 +76,3 @@ const Gameboard = (x,y) => {
 
 module.exports = Gameboard;
 
-// let x = Array(5);
-// for (let index = 0; index < 5; index++) {
-//   const array = Array(6);
-//   array.fill("");
-//   x[index] = array;
-  
-// }
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,7 +1,9 @@
 const gameboard = require("../src/gameboard");
 const ship = require("../src/ship");
 const newBoard = gameboard(7,6);
-const newShip = ship(3);
+const shipA = ship(3);
+const shipB = ship(3);
+const shipC = ship(2);
 
 it("gameboard x-dimension is ok?", () => {  
   expect(newBoard.board.length).toBe(7);
@@ -13,10 +15,10 @@ it("gameboard y-dimension is ok?", () => {
 })
 
 it("right ship placement (vertical)", () => {
-  expect(newBoard.placeShip(newShip, 2 , 3, "v")).toStrictEqual([
+  expect(newBoard.placeShip(shipA, 2 , 3, "v")).toStrictEqual([
     [ '', '', '', '', '', '' ],
     [ '', '', '', '', '', '' ],
-    [ '', '', '', 'SHIP', 'SHIP', 'SHIP' ],
+    [ '', '', '', shipA, shipA, shipA ],
     [ '', '', '', '', '', '' ],
     [ '', '', '', '', '', '' ],
     [ '', '', '', '', '', '' ],
@@ -25,10 +27,10 @@ it("right ship placement (vertical)", () => {
 } )
 
 it("right ship placement (horizontal)", () => {
-  expect(newBoard.placeShip(newShip, 0 , 0, "h")).toStrictEqual([
-    [ 'SHIP', '', '', '', '', '' ],
-    [ 'SHIP', '', '', '', '', '' ],
-    [ 'SHIP', '', '', 'SHIP', 'SHIP', 'SHIP' ],
+  expect(newBoard.placeShip(shipB, 0 , 0, "h")).toStrictEqual([
+    [ shipB, '', '', '', '', '' ],
+    [ shipB, '', '', '', '', '' ],
+    [ shipB, '', '', shipA, shipA, shipA ],
     [ '', '', '', '', '', '' ],
     [ '', '', '', '', '', '' ],
     [ '', '', '', '', '', '' ],
@@ -37,10 +39,88 @@ it("right ship placement (horizontal)", () => {
 } )
 
 it("wrong ship placement (horizontal)", () => {
-  expect(newBoard.placeShip(newShip, 8 , 0, "h")).toBe(false);
+  expect(newBoard.placeShip(shipC, 8 , 0, "h")).toBe(false);
 } )
 
 it("wrong ship placement (vertical)", () => {
-  expect(newBoard.placeShip(newShip, 0 , 7, "v")).toBe(false);
+  expect(newBoard.placeShip(shipC, 0 , 7, "v")).toBe(false);
 } )
 
+it("can't overplace ships horizontal", () => {
+  expect(newBoard.placeShip(shipC, 1 , 4, "h")).toStrictEqual([
+    [ shipB, '', '', '', '', '' ],
+    [ shipB, '', '', '', '', '' ],
+    [ shipB, '', '', shipA, shipA, shipA ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ]
+  ]);
+} )
+
+it("can't overplace ships vertical", () => {
+  expect(newBoard.placeShip(shipC, 1 , 0, "v")).toStrictEqual([
+    [ shipB, '', '', '', '', '' ],
+    [ shipB, '', '', '', '', '' ],
+    [ shipB, '', '', shipA, shipA, shipA ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ]
+  ]);  
+} )
+
+it("other ship", () => {
+  expect(newBoard.placeShip(shipC, 0 , 1, "v")).toStrictEqual([
+    [ shipB, shipC, shipC, '', '', '' ],
+    [ shipB, '', '', '', '', '' ],
+    [ shipB, '', '', shipA, shipA, shipA ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ]
+  ]);
+} )
+
+
+it("Attack board (HIT)", () => {
+  expect(newBoard.receiveAttack(0,2)).toStrictEqual(shipC);
+  expect(newBoard.board).toStrictEqual([
+    [ shipB, shipC, 'X', '', '', '' ],
+    [ shipB, '', '', '', '', '' ],
+    [ shipB, '', '', shipA, shipA, shipA ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ]
+  ]);
+  expect(shipC.isSunk()).toBe(false);
+} )
+
+
+it("Attack board (NO HIT)", () => {
+  expect(newBoard.receiveAttack(3,2)).toBe(false);
+  expect(newBoard.board).toStrictEqual([
+    [ shipB, shipC, 'X', '', '', '' ],
+    [ shipB, '', '', '', '', '' ],
+    [ shipB, '', '', shipA, shipA, shipA ],
+    [ '', '', 'X', '', '', '' ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ]
+  ]);
+} )
+
+it("Attacked board, hit shipC, is sunk", () => {
+  expect(newBoard.receiveAttack(0,1)).toStrictEqual(shipC);
+  expect(newBoard.board).toStrictEqual([
+    [ shipB, 'X', 'X', '', '', '' ],
+    [ shipB, '', '', '', '', '' ],
+    [ shipB, '', '', shipA, shipA, shipA ],
+    [ '', '', 'X', '', '', '' ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ],
+    [ '', '', '', '', '', '' ]
+  ]);
+  expect(shipC.isSunk()).toBe(true);
+} )
